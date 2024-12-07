f87c3963c76e1d56f1b7963b469472f24664cb98
Abin Babu <abinbabuonline@gmail.com>
2024-12-07 13:45:37 +0530

    user-use-cases

---
+++ server/auth-service/src/usecases/interface/usecase/user_use_case.ts
@@ -0,0 +1,8 @@
+import { Next } from '../../../framework/types/serverTypes';
+import { Iuser } from '../../../entities/user';
+import { Itoken } from '../service/jwt';
+export interface Iuser_use_case{
+    user_signup(user: Iuser, next: Next): Promise <string | void>
+    create_user(email: string, otp: string, next: Next): Promise <Iuser | void>
+    login(email: string, password: string, next: Next): Promise <{user: Iuser, token: Itoken} | void>
+}
