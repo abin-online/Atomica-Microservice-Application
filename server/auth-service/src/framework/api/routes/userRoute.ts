d9c23785dcfd413157379b9cea649567987211ba
Abin Babu <abinbabuonline@gmail.com>
2024-12-07 22:46:05 +0530

    injections

--- server/auth-service/src/framework/api/routes/userRoute.ts
+++ server/auth-service/src/framework/api/routes/userRoute.ts
@@ -1,1 +1,7 @@
 import { Next, Req, Res, Route } from "../../types/serverTypes";
+import { Iuser } from "../../../entities/user";
+export function user_route(router:  Route) {
+    router.post('/register', async(req: Req, res: Res, next: Next)=> {
+        
+    })
+}
