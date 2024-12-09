977004448dc5b2def91f61cf23a916bbc3b8c166
Abin Babu <abinbabuonline@gmail.com>
2024-12-10 00:01:24 +0530

    user routes

--- server/auth-service/src/framework/api/routes/userRoute.ts
+++ server/auth-service/src/framework/api/routes/userRoute.ts
@@ -1,7 +1,8 @@
 import { Next, Req, Res, Route } from "../../types/serverTypes";
 import { Iuser } from "../../../entities/user";
+import { UserController } from "../../../controller/userController";
 export function user_route(router:  Route) {
     router.post('/register', async(req: Req, res: Res, next: Next)=> {
-        
+        UserController.signup(req, res, next )
     })
 }
