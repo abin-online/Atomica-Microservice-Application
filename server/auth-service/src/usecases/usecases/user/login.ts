1e7f9259c1ab409e3a2b021429c17b6fbd4c8c97
Abin Babu <abinbabuonline@gmail.com>
2024-12-09 23:51:56 +0530

    login

---
+++ server/auth-service/src/usecases/usecases/user/login.ts
@@ -0,0 +1,27 @@
+import { Next } from "../../../framework/types/serverTypes";
+import { IuserRepository } from "../../interface/respositoryInterface/userRepository";
+import { IhashPassword } from "../../interface/service/hashPassword";
+import { IJwt } from "../../interface/service/jwt";
+import { catchError } from "../../middlewares/catchError";
+import ErrorHandler from "../../middlewares/errorHandler";
+
+
+export const login = async (userRepository: IuserRepository, jwt: IJwt, hashPassword: IhashPassword, email: string, password: string, next: Next): Promise<object | void> => {
+    try {
+        const user = await userRepository.findByEmail(email)
+        if (!user) {
+            return next(new ErrorHandler(400, 'invalid email id'))
+        }
+        if (user.is_blocked == true) {
+            return next(new ErrorHandler(400, 'access denied'))
+        }
+        const comparePassword = await hashPassword.compareHashPassword(password, user.password)
+        if (!comparePassword) {
+            return next(new ErrorHandler(400, 'incorrect password'))
+        }
+        const token: any = await jwt.create_access_and_refresh_token(user._id as string)
+        token.role = 'user'
+    } catch (error) {
+        throw error
+    }
+}
