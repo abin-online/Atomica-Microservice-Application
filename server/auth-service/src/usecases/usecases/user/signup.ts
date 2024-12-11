a55f0ac537c62924f0792e5932388f295ece5774
Abin Babu <abinbabuonline@gmail.com>
2024-12-11 22:54:56 +0530

    signup

--- server/auth-service/src/usecases/usecases/user/signup.ts
+++ server/auth-service/src/usecases/usecases/user/signup.ts
@@ -5,13 +5,20 @@ import { IotpRepository } from "../../interface/respositoryInterface/otpReposito
 import ErrorHandler from "../../middlewares/errorHandler";
 import { IuserRepository } from "../../interface/respositoryInterface/userRepository";
 import { catchError } from "../../middlewares/catchError";
-
-export const userSignup = async (jwt : IJwt, otpRepository: IotpRepository, userRepository: IuserRepository, user: Iuser, next: Next) : Promise <string | void> => {
+import { IotpGenerate } from "../../interface/service/otpGenerate";
+import { IhashPassword } from "../../interface/service/hashPassword";
+import { IsentEmail } from "../../interface/service/sentEmail";
+export const userSignup = async (jwt : IJwt, otpRepository: IotpRepository, userRepository: IuserRepository,otpGenerate:IotpGenerate, hashPassword:IhashPassword, user: Iuser, sentEmail:IsentEmail,  next: Next) : Promise <string | void> => {
     try {
         const userExist = await userRepository.findByEmail(user.email)
         if(userExist){
             return next(new ErrorHandler(400, 'user already exist'))
         }
+        const otp = await otpGenerate.createOtp()
+        await otpRepository.createOtp(user.email, otp)
+        await sentEmail.sentEmailVerification(user.name,user.email,otp)
+        const hashPasswords = await hashPassword.createHash(user.password as string)
+        user.password=hashPasswords
         const token = await jwt.create_verification_jwt({name: user.name, email: user.email, password: user.password})
         return token
     } catch (error) {
