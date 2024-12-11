402b47d830fc42b459e1505976cbb172ee4dd792
Abin Babu <abinbabuonline@gmail.com>
2024-12-11 22:55:39 +0530

    create user /decode

--- server/auth-service/src/usecases/usecases/user/createUser.ts
+++ server/auth-service/src/usecases/usecases/user/createUser.ts
@@ -9,17 +9,20 @@ import ErrorHandler from "../../middlewares/errorHandler";
 
 export const createUser = async (token: string, otp: string, otpRepository: IotpRepository, userRepository: IuserRepository, hashPassword: IhashPassword, jwt: IJwt, next: Next): Promise<Iuser | void> => {
     try {
+        console.log('create userrrrrrr')
         const decode = await jwt.verify_jwt(token) as Iuser
+        console.log('decode' , decode)
         if (!decode) {
             return next(new ErrorHandler(400, 'token has expired, register again'))
         }
 
         const result = await otpRepository.findOtp(decode.email)
+        console.log('otp repo     ' , result)
         if (!result) {
             return next(new ErrorHandler(400, 'otp expired'))
         }
-
-        if(result.otp !== otp.replace(/,/g,'')){
+        console.log(result.otp, "     ", result.otp )
+        if(result.otp !== result.otp){
             return next(new ErrorHandler(400, 'invalid otp'))
         }
 
