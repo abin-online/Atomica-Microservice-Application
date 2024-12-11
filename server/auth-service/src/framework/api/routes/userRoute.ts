343638a61358fa439d3b03c306585d3eb5c6a736
Abin Babu <abinbabuonline@gmail.com>
2024-12-11 22:58:16 +0530

    verify otp route

--- server/auth-service/src/framework/api/routes/userRoute.ts
+++ server/auth-service/src/framework/api/routes/userRoute.ts
@@ -1,8 +1,14 @@
 import { Next, Req, Res, Route } from "../../types/serverTypes";
 import { Iuser } from "../../../entities/user";
-import { UserController } from "../../../controller/userController";
-export function user_route(router:  Route) {
+import { userController } from "./injections/injections";
+
+export function UserRoute(router:  Route) {
+
     router.post('/register', async(req: Req, res: Res, next: Next)=> {
-        UserController.signup(req, res, next )
+        userController.signup(req, res, next )
+    })
+
+    router.post('/createUser', async(req: Req, res: Res, next: Next)=> {
+        userController.create_user(req, res, next)
     })
 }
