273ac3341fef7fbdd3106f036a4249cc468d9ce7
Abin Babu <abinbabuonline@gmail.com>
2024-12-12 17:22:04 +0530

    user route

--- server/auth-service/src/framework/api/routes/userRoute.ts
+++ server/auth-service/src/framework/api/routes/userRoute.ts
@@ -1,6 +1,8 @@
 import { Next, Req, Res, Route } from "../../types/serverTypes";
 import { Iuser } from "../../../entities/user";
 import { userController } from "./injections/injections";
+import { isUser } from "../middleware/roleAuth";
+
 
 export function UserRoute(router:  Route) {
 
@@ -10,5 +12,33 @@ export function UserRoute(router:  Route) {
 
     router.post('/createUser', async(req: Req, res: Res, next: Next)=> {
         userController.create_user(req, res, next)
+    })
+
+    router.post('/login' , async(req: Req, res: Res, next: Next) => {
+        userController.login(req, res, next)
+    })
+
+    router.post('/forgotPassword', async (req: Req, res: Res, next) => {
+        console.log('comming in forgot password', req.body)
+        userController.forgotPassword(req, res, next)
+    })
+
+    // router.post('/resendOtp' , async(req: Req, res: Res, next: Next) => {
+    //     userController.resendOTP(req, res, next)
+    // })
+    
+    router.post('/createNewPassword', async (req: Req, res: Res, next: Next) => {
+        console.log('comming in createNew password')
+        userController.createNewPassword(req, res, next)
+    })
+
+    router.post('/googleLogin', async (req: Req, res: Res, next: Next) => {
+        console.log('comming in google login')
+        userController.googleLogin(req, res, next);
+    })
+
+    router.post('/logout', isUser, async (req: Req, res: Res, next: Next) => {
+        console.log('comming into logout')
+        userController.logout(req, res, next);
     })
 }
