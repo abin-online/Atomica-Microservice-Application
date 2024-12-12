66889397fffecbf9ae9b660ef06c4605590f7052
Abin Babu <abinbabuonline@gmail.com>
2024-12-12 17:21:00 +0530

    token middleware

--- server/auth-service/src/framework/api/middleware/token.ts
+++ server/auth-service/src/framework/api/middleware/token.ts
@@ -3,9 +3,9 @@ const refresh_token_expire = parseInt(process.env.REFRESH_TOKEN_EXPIRE || '1200'
 
 interface Itoken_options {
     expires: Date,
-    max_age: number,
-    http_only: boolean,
-    same_site: 'lax' | 'strict' | 'none' | undefined
+    maxAge: number,
+    httpOnly: boolean,
+    sameSite: 'lax' | 'strict' | 'none' | undefined
     secure?: boolean
 }
 
@@ -13,24 +13,24 @@ const access_token_production_mode = process.env.NODE_ENV == 'production'
 
 export const access_token_options: Itoken_options = {
     expires: new Date(Date.now() + access_token_expire * 24 * 60 * 60 * 1000),
-    max_age: access_token_expire * 24 * 60 * 60 * 1000,
-    http_only: true,
-    same_site: 'none',
+    maxAge: access_token_expire * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+    sameSite: 'none',
     secure: access_token_production_mode
 }
 
 export const refresh_token_options: Itoken_options = {
     expires: new Date(Date.now() + refresh_token_expire * 24 * 60 * 60 * 1000),
-    max_age: refresh_token_expire * 24 * 60 * 60 * 1000,
-    http_only: true,
-    same_site: 'none',
+    maxAge: refresh_token_expire * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+    sameSite: 'none',
     secure: access_token_production_mode
 }
 
 export const role_options: Itoken_options = {
     expires: new Date(Date.now() + refresh_token_expire * 24 * 60 * 60 * 1000),
-    max_age: refresh_token_expire * 24 * 60 * 60 * 1000,
-    http_only: true,
-    same_site: 'none',
+    maxAge: refresh_token_expire * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+    sameSite: 'none',
     secure: access_token_production_mode
 }
