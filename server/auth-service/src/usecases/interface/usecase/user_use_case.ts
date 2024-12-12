1d2570582d5bab54b76c81476a6becdc9a70e453
Abin Babu <abinbabuonline@gmail.com>
2024-12-12 17:23:06 +0530

    user-use-case interface

--- server/auth-service/src/usecases/interface/usecase/user_use_case.ts
+++ server/auth-service/src/usecases/interface/usecase/user_use_case.ts
@@ -1,8 +1,11 @@
 import { Next } from '../../../framework/types/serverTypes';
 import { Iuser } from '../../../entities/user';
 import { Itoken } from '../service/jwt';
-export interface Iuser_use_case{
-    userSignup(user: Iuser, next: Next): Promise <string | void>
-    create_user(email: string, otp: string, next: Next): Promise <Iuser | void>
-    login(email: string, password: string, next: Next): Promise <{user: Iuser, token: Itoken} | void>
+export interface Iuser_use_case {
+    userSignup(user: Iuser, next: Next): Promise<string | void>
+    create_user(email: string, otp: string, next: Next): Promise<Iuser | void>
+    login(email: string, password: string, next: Next): Promise<{ user: Iuser, token: Itoken } | void>
+    forgotPasswordRemainder(email: string, next: Next): Promise<object | void>
+    googleLogin(name: string, email: string, passsword: string, next: Next): Promise<object | void>
+    emailVerify(email: string, otp: string, next: Next): Promise<object | void>
 }
