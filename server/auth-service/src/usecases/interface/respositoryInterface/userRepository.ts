63bf24ac5eea31883bc18531d0da2691a3ff116a
Abin Babu <abinbabuonline@gmail.com>
2024-12-12 17:22:29 +0530

    user repository

--- server/auth-service/src/usecases/interface/respositoryInterface/userRepository.ts
+++ server/auth-service/src/usecases/interface/respositoryInterface/userRepository.ts
@@ -4,4 +4,7 @@ export interface IuserRepository {
     createUser(newUser: Iuser) : Promise <Iuser>
     findByEmail(email: string) : Promise <Iuser | void>
     signup(email: string) : Promise <string>
+    getAllUser():Promise<Iuser[]|void>
+    blockUser(id:string):Promise<Iuser|void>
+    getUser(id:string):Promise<Iuser|undefined>
 }
