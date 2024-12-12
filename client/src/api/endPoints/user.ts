6b6be350f11341cf6eff81e5dea63d1d4b278ded
Abin Babu <abinbabuonline@gmail.com>
2024-12-12 17:36:03 +0530

    api folder

---
+++ client/src/api/endPoints/user.ts
@@ -0,0 +1,9 @@
+const userRoutes = {
+    signup: '/auth/user/register',
+    verifyOTP: '/auth/user/createUser',
+    login : '/auth/user/login',
+    googleLogin:'/user/googleLogin',
+    resendOtp : '/user/resendOtp'
+}
+
+export default userRoutes
---
+++ client/src/api/user.ts
@@ -0,0 +1,51 @@
+import { userData } from "@/@types/userData";
+import API from "@/service/axios";
+import userRoutes from "./endPoints/user";
+import { loginData } from "@/@types/loginType";
+
+
+export const signup = async (userData: userData) => {
+    try {
+        const response = await API.post(userRoutes.signup, userData)
+        return response.data
+    } catch (error: any) {
+        return error
+    }
+}
+
+export const verifyOTP = async (otp: string, email: string) => {
+    try {
+        const response = await API.post(userRoutes.verifyOTP, { otp, email })
+        console.log(response)
+        return response.data
+    } catch (error: any) {
+        return error
+    }
+}
+
+export const login = async (loginData: loginData) => {
+    try {
+        const response = await API.post(userRoutes.login, loginData)
+        return response.data
+    } catch (error: any) {
+        return error
+    }
+}
+
+export const userGoogleLogin = async (loginData:object)=>{
+    try {
+        const response = await API.post(userRoutes.googleLogin,loginData)
+        return response.data
+    } catch (error:any) {
+        return error
+    }
+}
+
+export const resendOtp = async (email : string)=> {
+    try {
+        const response = await API.post(userRoutes.resendOtp , email)
+        return response.data
+    } catch (error) {
+        return error
+    }
+}
