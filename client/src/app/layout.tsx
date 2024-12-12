9517bb635bf2c759e8cb49391f2663f17735d8b9
Abin Babu <abinbabuonline@gmail.com>
2024-12-12 17:09:13 +0530

    toast provider

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import ToastProvider from "./ToastProvider";
+import { Provider } from "react-redux";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,7 +30,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        
+        <ToastProvider>
+          {children}
+        </ToastProvider>
+
       </body>
     </html>
   );
