57db2d70b19904982e84eec39dfb343df6cdd235
Abin Babu <abinbabuonline@gmail.com>
2024-12-13 20:20:44 +0530

    forgot password

---
+++ server/auth-service/src/usecases/usecases/user/forgotPassword.ts
@@ -0,0 +1,24 @@
+import { Next } from "../../../framework/types/serverTypes";
+import { IotpRepository } from "../../interface/respositoryInterface/otpRepository";
+import { IuserRepository } from "../../interface/respositoryInterface/userRepository";
+import { IotpGenerate } from "../../interface/service/otpGenerate";
+import { IsentEmail } from "../../interface/service/sentEmail";
+import ErrorHandler from "../../middlewares/errorHandler";
+
+export const forgotPassword = async (sentEmail: IsentEmail, otpRepository: IotpRepository, otpGenerate: IotpGenerate, userRepository: IuserRepository, email: string, next: Next) => {
+    try {
+        const user = await userRepository.findByEmail(email)
+        if (!user) {
+            return next(new ErrorHandler(400, 'Invalid user'));
+
+        }
+        const otp = await otpGenerate.createOtp()
+        await otpRepository.createOtp(user.email, otp)
+        await sentEmail.sentEmailVerification(user.name, user.email, otp)
+        return {
+            message: 'OTP sent successfully'
+        }
+    } catch (error) {
+        throw error
+    }
+}
