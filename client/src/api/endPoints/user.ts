441ae239b012bf75f3ddc67198e6fe289395de5c
Abin Babu <abinbabuonline@gmail.com>
2024-12-14 23:29:47 +0530

    logout end point

--- client/src/api/endPoints/user.ts
+++ client/src/api/endPoints/user.ts
@@ -2,8 +2,9 @@ const userRoutes = {
     signup: '/auth/user/register',
     verifyOTP: '/auth/user/createUser',
     login : '/auth/user/login',
+    logOut : '/auth/user/logout',
     googleLogin:'/user/googleLogin',
-    resendOtp : '/user/resendOtp'
+    resendOtp : '/auth/user/resendOtp'
 }
 
 export default userRoutes
