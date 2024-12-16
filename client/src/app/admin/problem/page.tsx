9863985bf98a3dffb48e6329d6c0368cbd424d7d
Abin Babu <abinbabuonline@gmail.com>
2024-12-16 20:36:30 +0530

    admin problem management page

---
+++ client/src/app/admin/problem/page.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import React from 'react'
+import Header from '@/components/admin/Header'
+import Sidebar from '@/components/admin/SideBar'
+const Problem = () => {
+  return (
+    <div className="flex">
+    <Header />
+    <Sidebar />
+    </div>
+  )
+}
+
+export default Problem
