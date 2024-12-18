6a25cc909f2c574ff9d92e79d826160fb1d4900f
Abin Babu <abinbabuonline@gmail.com>
2024-12-18 13:07:10 +0530

    admin endpoints

--- client/src/api/endPoints/admin.ts
+++ client/src/api/endPoints/admin.ts
@@ -1,7 +1,9 @@
 
 const adminRoutes = {
+    login: '/auth/admin/adminLogin',
     getUsers : '/auth/admin/get-users',
-    blockUser : '/auth/admin/block-user'
+    blockUser : '/auth/admin/block-user',
+    logout: '/auth/admin/adminLogout'
 }
 
 export default adminRoutes
