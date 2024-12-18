5f2bff4f450317d9f73729b1405d6f58069fe4c7
Abin Babu <abinbabuonline@gmail.com>
2024-12-18 13:12:14 +0530

    admin logout UI

--- client/src/components/admin/SideBar.tsx
+++ client/src/components/admin/SideBar.tsx
@@ -3,9 +3,17 @@
 // components/Sidebar.js
 import { useState } from 'react';
 import Link from 'next/link';
-import { FiHome, FiUsers,FiArrowRightCircle,FiArrowLeftCircle,  FiLogOut, FiAlertTriangle, FiOctagon } from 'react-icons/fi';
-
+import { FiHome, FiUsers, FiArrowRightCircle, FiArrowLeftCircle, FiLogOut, FiAlertTriangle, FiOctagon } from 'react-icons/fi';
+import { adminLogout } from '@/api/admin';
+import { useAppDispatch } from '@/lib/hook';
+import { removeUser } from '@/lib/features/users/userSlice';
+import { removeAdmin } from '@/lib/features/users/adminSlice';
+import toast from 'react-hot-toast';
+import { useRouter } from 'next/navigation';
 const Sidebar = () => {
+  const router = useRouter()
+  const dispatch = useAppDispatch()
+
   const [isOpen, setIsOpen] = useState(true);
 
   const toggleSidebar = () => {
@@ -17,14 +25,31 @@ const Sidebar = () => {
     { name: 'Users', icon: <FiUsers />, path: '/admin/users' },
     { name: 'QuickTest', icon: <FiOctagon />, path: '/admin/quickTest' },
     { name: 'Problem', icon: <FiAlertTriangle />, path: '/admin/problem' },
-    { name: 'Logout', icon: <FiLogOut />, path: '/logout' },
   ];
+
+  const handleLogout = async ()=> {
+    try {
+      const response = await adminLogout()
+      console.log('admin logout response = >' , response)
+      
+      if(response?.message == 'logout success') {
+        dispatch(removeAdmin());
+        toast.success('logout success')
+        router.push('/admin')
+      }else{
+        toast.error('An error occured')
+      }
+      
+
+    } catch (error) {
+      
+    }
+  }
 
   return (
     <div
-      className={`bg-gray-800 text-white h-screen p-5 pt-8 ${
-        isOpen ? 'w-64' : 'w-16'
-      } duration-300 relative shadow-2xl`} // Added shadow here
+      className={`bg-gray-800 text-white h-screen p-5 pt-8 ${isOpen ? 'w-64' : 'w-16'
+        } duration-300 relative shadow-2xl`} // Added shadow here
     >
       {/* Logo */}
       <div className="flex items-center gap-x-4">
@@ -33,30 +58,45 @@ const Sidebar = () => {
           {isOpen ? '◁' : '▷'}
         </button>
       </div>
-  
+
       {/* Menu */}
       <ul className="mt-10">
         {menuItems.map((item, index) => (
           <Link key={index} href={item.path}>
-          <li
-            
-            className="flex items-center gap-x-4 p-2 my-4 hover:bg-gray-700 rounded-lg cursor-pointer"
-          >
-            <div className="text-2xl">{item.icon}</div>
-            
+            <li
+
+              className="flex items-center gap-x-4 p-2 my-4 hover:bg-gray-700 rounded-lg cursor-pointer"
+            >
+              <div className="text-2xl">{item.icon}</div>
+
               <span
-                className={`text-base font-medium ${
-                  isOpen ? '' : 'hidden'
-                } duration-700 relative`}
+                className={`text-base font-medium ${isOpen ? '' : 'hidden'
+                  } duration-700 relative`}
               >
                 {item.name}
               </span>
-            
-          </li>
+
+            </li>
           </Link>
         ))}
+
+        <li onClick={handleLogout}
+
+          className="flex items-center gap-x-4 p-2 my-4 hover:bg-gray-700 rounded-lg cursor-pointer"
+        >
+          <div className="text-2xl"><FiLogOut /></div>
+
+          <span
+            className={`text-base font-medium ${isOpen ? '' : 'hidden'
+              } duration-700 relative`}
+          >
+            Logout
+          </span>
+
+        </li>
+
       </ul>
-  
+
       {/* Toggle Button */}
       <div className="flex items-center gap-x-4">
         <button onClick={toggleSidebar} className="text-4xl">
@@ -65,7 +105,7 @@ const Sidebar = () => {
       </div>
     </div>
   );
-  
+
 };
 
 export default Sidebar;
