3637321dd57d659290ee315073e3af21a245e5dc
Abin Babu <abinbabuonline@gmail.com>
2024-12-20 17:14:30 +0530

    admin problem management

--- client/src/app/admin/problem/page.tsx
+++ client/src/app/admin/problem/page.tsx
@@ -1,9 +1,24 @@
 'use client'
 
-import React from 'react'
+import React, { useEffect } from 'react'
 import Header from '@/components/admin/Header'
 import Sidebar from '@/components/admin/SideBar'
+import { useAppDispatch } from '@/lib/hook'
+import { setAdmin } from '@/lib/features/users/adminSlice'
+import { useRouter } from 'next/navigation'
 const Problem = () => {
+  
+      const dispatch = useAppDispatch()
+      
+      const router = useRouter()
+      useEffect(() => {
+          const storedAdmin = localStorage.getItem('admin');
+          if (storedAdmin) {
+            dispatch(setAdmin(JSON.parse(storedAdmin)));
+          }else{
+            router.push(`/admin`)
+        }
+        }, [dispatch]);
   return (
     <div className="flex">
     <Header />
