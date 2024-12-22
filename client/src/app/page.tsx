1b1946ee49ae8eddc25567474b50063e965b525c
Abin Babu <abinbabuonline@gmail.com>
2024-12-22 22:43:28 +0530

    Navbar

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,8 +1,6 @@
 'use client'
 
 import React, { useState, useEffect } from 'react';
-// import { FaUserCircle } from 'react-icons/fa';
-// import { FiMenu, FiX } from 'react-icons/fi';
 
 import Link from 'next/link';
 import { useAppDispatch, useAppSelector } from '@/lib/hook';
@@ -12,135 +10,14 @@ import { useRouter } from 'next/navigation';
 import { logOut } from '@/api/user';
 import toast from 'react-hot-toast';
 import { useConfirmationDialog } from './customHooks/useConfirmationDialog';
+import NavBar from '@/components/user/Navbar';
 
-const NavBar = () => {
-
-  const { dialog, openDialog } = useConfirmationDialog();
-
-  const router = useRouter(); 
-  const user = useAppSelector((state: RootState) => state.user);
-  const [btnNameReact, setBtnNameReact] = useState(user.id ? 'LOGOUT' : 'LOGIN');
-  const dispatch = useAppDispatch()
-  useEffect(() => {
-    // Update button name whenever the user's login state changes
-    setBtnNameReact(user.id ? 'LOGOUT' : 'LOGIN');
-  }, [user.id]);
-
-
-  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);  // State for toggling menu
-
-  const handleLogOut = async ()=> {
-
-   const response = await logOut()
-    if(response.message == 'logout success') {
-      console.log('user logouted',dispatch(removeUser()))
-      toast.success('Logged out successfully')
-    }else{
-      toast.error('Something error')
-    }
-
-
-
-  }
-
-  const confirmLogOut = () => {
-    openDialog({
-      title: "Logout Confirmation",
-      message: "Are you sure you want to log out?",
-      onConfirm: handleLogOut,
-    });
-  };
-
+const HomePage = ()=> {
   return (
-    <div className="flex justify-between sm:justify-start shadow-lg mb-1 p-4">
-      {/* Logo - Center in mobile, Left in desktop */}
-      {/* <div className="logo-container sm:mr-auto text-center p-4 sm:text-left">
-        <p className="text-2xl font-bold">ATOMICA</p>
-      </div> */}
-  
-      {/* Desktop Navigation */}
-      <div className="sm:flex hidden">
-        <div className="flex items-center">
-          <ul className="flex p-4 m-3 gap-10 text-base font-medium">
-            <li className="transform hover:scale-105 transition duration-300">
-              <Link href="/">ATOMICA</Link>
-            </li>
-            <li className="transform hover:scale-105 transition duration-300">
-              <Link href="/about">ABOUT US</Link>
-            </li>
-            <li className="transform hover:scale-105 transition duration-300">
-              <Link href="/contact">QUICK TEST</Link>
-            </li>
-            <li className="transform hover:scale-105 transition duration-300">
-              <Link href="/grocery">PROBLEM</Link>
-            </li>
-            <button
-                className=" border-black mr-auto"
-                onClick={() => {
-                  if (user.id) {
-                    
-                    confirmLogOut();
-                  } else {
-                    
-                    router.push('/login'); // Redirect to /login route
-                  }
-                }}
-              >
-                {btnNameReact}
-              </button>
-          </ul>
-        </div>
-      </div>
-  
-      {/* Mobile Navigation */}
-      <div className="sm:hidden flex items-center relative">
-        <button
-          className="text-2xl p-4"
-          onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}  // Toggle mobile menu
-        >
-          ☰
-        </button>
-  
-        {/* Mobile Menu */}
-        {isMobileMenuOpen && (
-          <div className="absolute top-0 left-0 bg-white w-48 shadow-lg mt-16">
-            <ul className="flex flex-col p-4 gap-6 text-xl font-medium">
-              <li className="transform hover:scale-105 transition duration-300">
-                <Link href="/about">ABOUT US</Link>
-              </li>
-              <li className="transform hover:scale-105 transition duration-300">
-                <Link href="/contact">QUICK TEST</Link>
-              </li>
-              <li className="transform hover:scale-105 transition duration-300">
-                <Link href="/grocery">PROBLEM</Link>
-              </li>
-              <li className="transform hover:scale-105 transition duration-300">
-              <button
-                
-                onClick={() => {
-                  if (user.id) {
-                    
-                    confirmLogOut();
-                  } else {
-                    
-                    router.push('/login'); // Redirect to /login route
-                  }
-                }}
-              >
-                {btnNameReact}
-              </button>
-
-              </li>
-   
-            </ul>
-          </div>
-        )}
-      </div>
-      {dialog}
-    </div>
-  );
-  
-
+    <>
+    <NavBar/>
+    </>
+  )
 }
 
-export default NavBar;
+export default HomePage;
