67834bb7907a2824d457d7f41331183345865282
Abin Babu <abinbabuonline@gmail.com>
2024-12-22 22:43:16 +0530

    ROUTE

--- client/src/api/endPoints/user.ts
+++ client/src/api/endPoints/user.ts
@@ -1,10 +1,12 @@
+
 const userRoutes = {
     signup: '/auth/user/register',
     verifyOTP: '/auth/user/createUser',
     login : '/auth/user/login',
     logOut : '/auth/user/logout',
     googleLogin:'/auth/user/googleLogin',
-    resendOtp : '/auth/user/resendOtp'
+    resendOtp : '/auth/user/resendOtp',
+    forgotPassword : '/auth/user/password'
 }
 
 export default userRoutes
