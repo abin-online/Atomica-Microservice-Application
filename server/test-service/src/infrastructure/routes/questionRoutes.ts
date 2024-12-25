f994c4b9c4c38a24590a188c3d9270d3654cc7bd
Abin Babu <abinbabuonline@gmail.com>
2024-12-25 12:40:18 +0530

    Q routes

---
+++ server/test-service/src/infrastructure/routes/questionRoutes.ts
@@ -0,0 +1,27 @@
+import { Router } from "express";
+import { mcqController } from "./injection";
+import { Req, Res, Next } from "../../interface/framework/types/serverTypes";
+
+export const QuestionRoute = (router: Router) => {
+  router.post("/addQuestion", (req: Req, res: Res, next: Next) =>
+    mcqController.createQuestion(req, res, next)
+  );
+  router.get("/getAllQuestions", (req: Req, res: Res, next: Next) =>
+    mcqController.getAllQuestions(req, res, next)
+  );
+  router.put("/blockQuestion", (req: Req, res: Res, next: Next) =>
+    mcqController.blockQuestion(req, res, next)
+  );
+  router.get("/getQuestion/:id", (req: Req, res: Res, next: Next) =>
+    mcqController.getEditQuestion(req, res, next)
+  );
+  router.put("/updateQuestion/:id", (req: Req, res: Res, next: Next) =>
+    mcqController.updateQuestion(req, res, next)
+  );
+
+  router.get('/getAllMcq', (req: Req, res: Res, next : Next) => 
+    mcqController.getMCQ(req, res, next)
+);
+
+  return router;
+};
