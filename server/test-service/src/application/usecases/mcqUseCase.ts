7b2457fa33f283b83d47e26a46e4dd0c6c518151
Abin Babu <abinbabuonline@gmail.com>
2024-12-25 12:45:39 +0530

    mcq usecase

---
+++ server/test-service/src/application/usecases/mcqUseCase.ts
@@ -0,0 +1,39 @@
+import { Request, Response, NextFunction } from "express";
+import { IMcqUseCase } from "../interfaces/useCaseInterfaces/mcqUseCaseInterface";
+import ErrorHandler from "../../application/usecases/middleware/errorHandler";
+import { IMcqRepository } from "../interfaces/repositoryInterfaces/testRepositoryInterface";
+import { ObjectId } from "mongoose";
+
+
+export default class McqUseCase implements IMcqUseCase {
+  private mcqRepository: IMcqRepository;
+
+  constructor(mcqRepository: IMcqRepository) {
+    this.mcqRepository = mcqRepository;
+  }
+
+  async createQuestion(data: any): Promise<any> {
+    console.log(data)
+    return await this.mcqRepository.create(data);
+  }
+
+  async getAllQuestions(): Promise<any[]> {
+    return await this.mcqRepository.findAll();
+  }
+
+  async blockQuestion(questionId: string, blocked: boolean): Promise<any> {
+    return await this.mcqRepository.updateBlockedStatus(questionId, blocked);
+  }
+
+  async getQuestionById(id: string): Promise<any> {
+    return await this.mcqRepository.findById(id);
+  }
+
+  async updateQuestion(id: string, data: any): Promise<any> {
+    return await this.mcqRepository.updateById(id, data);
+  }
+
+  async getMCQ(tag: string, difficulty: string): Promise<any[]> {
+    return await this.mcqRepository.fetchToUser(tag, difficulty);
+  }
+}
