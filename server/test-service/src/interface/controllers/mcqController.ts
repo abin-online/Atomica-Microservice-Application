5a0d251a13bd3050c7c6ecd9e45adde6a0ea596e
Abin Babu <abinbabuonline@gmail.com>
2024-12-25 12:39:50 +0530

    mcq controller

---
+++ server/test-service/src/interface/controllers/mcqController.ts
@@ -0,0 +1,96 @@
+import { Request, Response, NextFunction } from "express";
+import { IMcqUseCase } from "../../application/interfaces/useCaseInterfaces/mcqUseCaseInterface";
+import ErrorHandler from "../../application/usecases/middleware/errorHandler";
+import { error } from "console";
+import { isObjectIdOrHexString } from "mongoose";
+
+export default class McqController {
+  private mcqUseCase: IMcqUseCase;
+
+  constructor(mcqUseCase: IMcqUseCase) {
+    this.mcqUseCase = mcqUseCase;
+    this.createQuestion = this.createQuestion.bind(this);
+    this.getAllQuestions = this.getAllQuestions.bind(this);
+    this.blockQuestion = this.blockQuestion.bind(this);
+    this.getEditQuestion = this.getEditQuestion.bind(this);
+    this.updateQuestion = this.updateQuestion.bind(this);
+    this.getMCQ = this.getMCQ.bind(this);
+  }
+
+  async createQuestion(req: Request, res: Response, next: NextFunction): Promise<void> {
+    try {
+      const { question, options, correctAnswer, difficulty, tags } = req.body;
+      console.log(req.body)
+      if (!question || !options || !correctAnswer || !difficulty || !tags) {
+        res.status(400).json({ error: "All fields are required" });
+      }
+      const newMCQ = await this.mcqUseCase.createQuestion({ question, options, correctAnswer, difficulty, tags });
+      res.status(201).json({ message: "Question created", question: newMCQ });
+    } catch (error: any) {
+      return next(new ErrorHandler(error.status, error.message));
+    }
+  }
+
+  async getAllQuestions(req: Request, res: Response, next: NextFunction): Promise<void> {
+    try {
+      const questions = await this.mcqUseCase.getAllQuestions();
+      res.status(200).json(questions);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async blockQuestion(req: Request, res: Response, next: NextFunction): Promise<void> {
+    try {
+      const { questionId, blocked } = req.body;
+      const updatedQuestion = await this.mcqUseCase.blockQuestion(questionId, blocked);
+      if (!updatedQuestion) {
+         res.status(404).json({ error: "Question not found" });
+      }
+      res.status(200).json(updatedQuestion);
+    } catch (error: any) {
+      return next(new ErrorHandler(error.status, error.message));
+    }
+  }
+
+  async getEditQuestion(req: Request, res: Response, next: NextFunction): Promise<void> {
+    try {
+      const { id } = req.params;
+      const question = await this.mcqUseCase.getQuestionById(id);
+      if (!question) {
+        res.status(404).json({ error: "Question not found" });
+      }
+      res.status(200).json(question);
+    } catch (error: any) {
+      return next(new ErrorHandler(error.status, error.message));
+    }
+  }
+
+  async updateQuestion(req: Request, res: Response, next: NextFunction): Promise<void> {
+    try {
+      const { id } = req.params;
+      const { question, options, correctAnswer, difficulty, tags } = req.body;
+      const updatedQuestion = await this.mcqUseCase.updateQuestion(id, { question, options, correctAnswer, difficulty, tags });
+      if (!updatedQuestion) {
+        res.status(404).json({ error: "Question not found" });
+      }
+      res.status(200).json(updatedQuestion);
+    } catch (error: any) {
+      return next(new ErrorHandler(error.status, error.message));
+    }
+  }
+
+  async getMCQ(req: Request, res: Response, next: NextFunction) : Promise<void> {
+    try {
+      console.log("Query_______",req.query)
+      const {difficulty, tag} = req.query;
+      const mcq = await this.mcqUseCase.getMCQ(tag as string, difficulty as string);
+      if(!mcq) {
+        res.status(404).json({error: 'mcqs not found  '})
+      }
+      res.status(200).json(mcq)
+    } catch (error: any) {
+      return next(new ErrorHandler(error.status, error.message))
+    }
+  }
+}
