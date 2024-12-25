389c0deb867c66d7298f9f0e89256317397acc7d
Abin Babu <abinbabuonline@gmail.com>
2024-12-25 12:46:57 +0530

    test service APP

---
+++ server/test-service/src/index.ts
@@ -0,0 +1,41 @@
+import express from 'express'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+import dotenv from 'dotenv';
+dotenv.config();
+import { QuestionRoute } from './infrastructure/routes/questionRoutes';
+//import { errorMiddleware } from './usecases/middlewares/errorMiddleware'
+import { TagRoute } from './infrastructure/routes/tagRoutes';
+
+import connectDb from './infrastructure/database/db';
+connectDb()
+
+
+const app = express()
+
+
+const questionRouter = express.Router()
+const tagRouter = express.Router()
+TagRoute(tagRouter)
+QuestionRoute(questionRouter)
+
+app.use(cors({
+    credentials: true,
+    origin: process.env.CORS_ORIGIN
+}))
+
+app.use(cookieParser())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use('/mcq', questionRouter)
+app.use('/tag', tagRouter)
+
+
+const PORT = process.env.PORT || 5001
+
+app.listen(PORT, () => {
+    console.log(`Authentication server is running at http://localhost:${PORT}`);
+})
+
+//http://localhost:5001/tag/addTag
