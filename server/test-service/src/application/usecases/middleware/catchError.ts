2839bfbe90ad7d7b30dea0ae24917dab8b4f6bb8
Abin Babu <abinbabuonline@gmail.com>
2024-12-25 12:45:15 +0530

    catch err

---
+++ server/test-service/src/application/usecases/middleware/catchError.ts
@@ -0,0 +1,19 @@
+import { Next } from "../../../interface/framework/types/serverTypes";
+import ErrorHandler from "./errorHandler";
+
+export const catchError = (error: unknown, next: Next): void => {
+    console.log('catch error');
+    let message: string;
+    
+    if (error instanceof Error) {
+        message = error.message;
+    } else if (error && typeof error === 'object' && 'message' in error) {
+        message = String((error as { message: string }).message);
+    } else if (typeof error === 'string') {
+        message = error;
+    } else {
+        message = 'Unknown error';
+    }
+    
+    return next(new ErrorHandler(500, message));
+};
