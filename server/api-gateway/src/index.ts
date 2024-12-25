871e17f4620f8b6b49fc9de32a5ec719dfed4321
Abin Babu <abinbabuonline@gmail.com>
2024-12-25 12:51:07 +0530

    test service added to api gateway

--- server/api-gateway/src/index.ts
+++ server/api-gateway/src/index.ts
@@ -4,11 +4,17 @@ import { createProxyMiddleware } from "http-proxy-middleware"
 const app = express()
 
 const services = {
-    auth: 'http://localhost:5000'
+    auth: 'http://localhost:5000',
+    test: 'http://localhost:5001'
 }
 
 app.use('/auth', createProxyMiddleware({
     target: services.auth,
+    changeOrigin: true
+}))
+
+app.use('/mcq', createProxyMiddleware({
+    target: services.test,
     changeOrigin: true
 }))
 
