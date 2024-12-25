bd0452e2459eac53556bc4aa1bc3ee26ceb6b355
Abin Babu <abinbabuonline@gmail.com>
2024-12-25 12:45:26 +0530

    tag usecase

---
+++ server/test-service/src/application/usecases/tagUseCases.ts
@@ -0,0 +1,28 @@
+// src/application/usecases/tagUseCase.ts
+import { ITagUseCase } from "../interfaces/useCaseInterfaces/tagUseCaseInterface";
+import { ITagRepository } from "../interfaces/repositoryInterfaces/tagRepositoryInterface";
+import { ITag } from "../../domain/entities/tag";
+
+export default class TagUseCase implements ITagUseCase {
+  private tagRepository: ITagRepository;
+
+  constructor(tagRepository: ITagRepository) {
+    this.tagRepository = tagRepository;
+  }
+
+  async createTag(tag: ITag): Promise<ITag> {
+    if (!tag.name) {
+      throw new Error("Tag name is required");
+    }
+    return await this.tagRepository.create(tag);
+  }
+
+  async blockTag(id: string, blocked: boolean): Promise<ITag | null> {
+    console.log("from usecase     ",id, blocked)
+    return await this.tagRepository.blockTag(id, blocked);
+  }
+
+  async getAllTags(): Promise<ITag[]> {
+    return await this.tagRepository.getAll();
+  }
+}
