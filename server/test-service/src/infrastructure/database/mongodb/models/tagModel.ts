68e520726824bf665cdf8b495f56e02e6b909c41
Abin Babu <abinbabuonline@gmail.com>
2024-12-25 12:41:07 +0530

    tag Model

---
+++ server/test-service/src/infrastructure/database/mongodb/models/tagModel.ts
@@ -0,0 +1,10 @@
+import { Model, model, Schema } from "mongoose";
+import { ITag } from "../../../../domain/entities/tag";
+
+const tagSchema: Schema<ITag> = new Schema({
+  name: { type: String, required: true },
+  blocked: { type: Boolean, default: false }
+});
+
+const tagModel: Model<ITag> = model("tag", tagSchema);
+export default tagModel;
