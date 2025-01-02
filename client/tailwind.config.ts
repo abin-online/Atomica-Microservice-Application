bf206ebec8463da1f129f3b1d28f8f2e94f92af2
Abin Babu <abinbabuonline@gmail.com>
2025-01-02 09:55:00 +0530

    TW

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -1,3 +1,22 @@
+// import type { Config } from "tailwindcss";
+
+// export default {
+//   content: [
+//     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+//     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+//     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+//   ],
+//   theme: {
+//     extend: {
+//       colors: {
+//         background: "var(--background)",
+//         foreground: "var(--foreground)",
+//       },
+//     },
+//   },
+//   plugins: [],
+// } satisfies Config;
+
 import type { Config } from "tailwindcss";
 
 export default {
@@ -11,6 +30,20 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+      },
+      animation: {
+        'modal-fade-in': 'fade-in 0.5s ease-in-out',
+        'scale-up': 'scale-up 0.5s ease-out',
+      },
+      keyframes: {
+        'fade-in': {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 },
+        },
+        'scale-up': {
+          '0%': { transform: 'scale(0.5)' },
+          '100%': { transform: 'scale(1)' },
+        },
       },
     },
   },
