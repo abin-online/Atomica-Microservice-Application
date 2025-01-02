d04957532706154fdbaf968d3dc110296e683e57
Abin Babu <abinbabuonline@gmail.com>
2025-01-02 10:11:09 +0530

    kafka config - auth Service

---
+++ server/auth-service/src/framework/api/config/kafka/producer.ts
@@ -0,0 +1,28 @@
+import kafka from "./kafkaConfig";
+import { Partitioners } from "kafkajs";
+
+async function produce(topic: string, value: any | Buffer): Promise<void> {
+    try {
+        const producer = kafka.producer({createPartitioner : Partitioners.LegacyPartitioner}) 
+        //LegacyPartitioner, which decides how messages are partitioned across Kafka's topics.
+        console.log("Connecting to Kafka producer...");
+        await producer.connect();
+        console.log("Producer connected successfully.");
+        const messageValue = typeof value === "object" ? JSON.stringify(value) : value;
+
+        console.log(`Sending message to topic: ${topic} => ${messageValue}`);
+        await producer.send({
+            topic,
+            messages: [{ value: messageValue }],
+        });
+        
+        console.log("Message sent successfully.");
+
+        await producer.disconnect();
+        console.log("Producer disconnected.");
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export default produce
--- server/auth-service/src/usecases/usecases/user/createUser.ts
+++ server/auth-service/src/usecases/usecases/user/createUser.ts
@@ -1,4 +1,5 @@
 import { Iuser } from "../../../entities/user";
+import produce from "../../../framework/api/config/kafka/producer";
 import { Next } from "../../../framework/types/serverTypes";
 import { IotpRepository } from "../../interface/respositoryInterface/otpRepository";
 import { IuserRepository } from "../../interface/respositoryInterface/userRepository";
@@ -11,22 +12,27 @@ export const createUser = async (token: string, otp: string, otpRepository: Iotp
     try {
         console.log('create userrrrrrr')
         const decode = await jwt.verify_jwt(token) as Iuser
-        console.log('decode' , decode)
+        console.log('decode', decode)
         if (!decode) {
             return next(new ErrorHandler(400, 'token has expired, register again'))
         }
 
         const result = await otpRepository.findOtp(decode.email)
-        console.log('otp repo     ' , result)
+        console.log('otp repo     ', result)
         if (!result) {
             return next(new ErrorHandler(400, 'otp expired'))
         }
-        console.log(result.otp, "     ", otp )
-        if(result.otp !== otp){
+        console.log(result.otp, "     ", otp)
+        if (result.otp !== otp) {
             return next(new ErrorHandler(400, 'invalid otp'))
         }
 
         const newUser = await userRepository.createUser(decode)
+        console.log('decode data for add-user topic _______', decode)
+        
+        //---------------------------------------------
+        await produce('add-user', decode) //kafka produce |
+        //---------------------------------------------
         await otpRepository.findAndDeleteUser(decode.email)
         return newUser
     } catch (error) {
