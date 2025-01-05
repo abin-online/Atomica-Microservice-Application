1267693a59b8fc13b359ceea26b3030edae50a3d
Abin Babu <abinbabuonline@gmail.com>
2025-01-05 19:50:17 +0530

    Gateway Configuration ⚙️

--- server/api-gateway/src/index.ts
+++ server/api-gateway/src/index.ts
@@ -1,36 +1,57 @@
 import express from "express"
 import { createProxyMiddleware } from "http-proxy-middleware"
+import dotenv from 'dotenv'
+dotenv.config()
 
 const app = express()
 
-const services = {
-    auth: 'http://localhost:5000',
-    test: 'http://localhost:5001',
-    problem: 'http://localhost:5002',
-    badge: 'http://localhost:5003'
+const {
+    PORT,
+    AUTH_SERVICE,
+    TEST_SERVICE,
+    PROBLEM_SERVICE,
+    BADGE_SERVICE,
+    COMPILER_SERVICE,
+    SECURITY_NUMBER,
+    COMMAND
+} = process.env
+
+const services = [{
+    path: AUTH_SERVICE,
+    route: '/auth'
+}, {
+    path: TEST_SERVICE,
+    route: '/test'
+}, {
+    path: PROBLEM_SERVICE,
+    route: '/problem'
+}, {
+    path: BADGE_SERVICE,
+    route: '/badge'
+}, {
+    path: COMPILER_SERVICE,
+    route: '/problem'
 }
+]
 
-app.use('/auth', createProxyMiddleware({
-    target: services.auth,
-    changeOrigin: true
-}))
+const BOOLEAN = 1+2 == Number(SECURITY_NUMBER)
 
-app.use('/mcq', createProxyMiddleware({
-    target: services.test,
-    changeOrigin: true
-}))
-app.use('/badge', createProxyMiddleware({
-    target: services.badge,
-    changeOrigin: true
-}))
+app.use((req, res, next) => {
+    console.log(`LOGGING 📝 : ${req.method} request to: ${req.originalUrl}`);
+    next(); 
+});
 
-app.use('/problem', createProxyMiddleware({
-    target: services.problem,
-    changeOrigin: true
-}))
+services.forEach((service) => {
+    app.use(service?.route, createProxyMiddleware({
+        target: service?.path,
+        changeOrigin: BOOLEAN
+    }))
+})
 
-const PORT = 4001
+app.get('/', (req, res)=>{
+    res.json(COMMAND)
+})
 
-app.listen(PORT, ()=> {
-    console.log(`ATOMICA IS RUNNNING ON http://locahost:${PORT}`);
+app.listen(PORT, () => {
+    console.log(`ATOMICA RUNNING ON http://locahost:${PORT} 🍃`);
 })
