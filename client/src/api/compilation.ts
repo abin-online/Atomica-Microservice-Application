4aa1f59138cd25706c33b9325778b149c6f6f64d
Abin Babu <abinbabuonline@gmail.com>
2025-01-12 23:31:26 +0530

    COMPILER API

--- client/src/api/compilation.ts
+++ client/src/api/compilation.ts
@@ -1,11 +1,22 @@
-import API from "@/service/axios";
 import compiler from "./endPoints/compilation";
+import USERAPI from "@/service/axios";
 
-export const submitCode = async (code: any, language: string, )=> {
+export const submitCode = async (code: any, problem: string, language: string, )=> {
     try {
         console.log(code, language)
-        const data = JSON.stringify({code, language})
-        const response = await API.post(compiler.run, data );
+        const data = JSON.stringify({code, problem, language})
+        const response = await USERAPI.post(compiler.submit, data );
+        return response
+    } catch (error) {
+        
+    }
+}
+
+export const runCode = async (code: any, problem: string, language: string, )=> {
+    try {
+        console.log(code, language)
+        const data = JSON.stringify({code, problem, language})
+        const response = await USERAPI.post(compiler.run, data );
         return response
     } catch (error) {
         
