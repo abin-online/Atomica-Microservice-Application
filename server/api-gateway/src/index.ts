9ba92858dcfc6ca391ba376b786ee28051115e99
Abin Babu <abinbabuonline@gmail.com>
2025-02-08 20:03:44 +0530

    debug

--- server/api-gateway/src/index.ts
+++ server/api-gateway/src/index.ts
@@ -60,7 +60,6 @@ const BOOLEAN = 1 + 2 == Number(SECURITY_NUMBER)
 
 services.forEach((service) => {
     const isCollaboration = service.route === '/collaboration'
-    console.log('services route ',service.path)
     app.use(service?.route, createProxyMiddleware({
         target: service?.path,
         changeOrigin: BOOLEAN,
@@ -88,5 +87,5 @@ app.get('/services', (req, res) => {
 })
 
 app.listen(PORT, () => {
-    console.log(`ATOMICA RUNNING ON http://locahost:${PORT} 🍃`);
+    console.log(`ATOMICA RUNNING ON= http://locahost:${PORT} 🍃`);
 })
