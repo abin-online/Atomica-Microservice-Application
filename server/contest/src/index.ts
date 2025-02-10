ac1adc1800f1a4b24f41669d91fca3354560cf9d
Abin Babu <abinbabuonline@gmail.com>
2025-02-10 12:14:15 +0530

    fix : cors origin url

--- server/contest/src/index.ts
+++ server/contest/src/index.ts
@@ -14,7 +14,7 @@ const contestRouter = express.Router()
 ContestRoute(contestRouter);
 
 const corsOptions = {
-    origin: 'http://localhost:3000',
+    origin: 'http://35.200.186.106/',
     credentials: true
 }
 
