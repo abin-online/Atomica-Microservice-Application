ca06aa40ee75fdbf48e0878ade161c79d97d8f1e
Abin Babu <abinbabuonline@gmail.com>
2025-02-11 18:40:47 +0530

    feat : client socket url

--- client/src/lib/socket.ts
+++ client/src/lib/socket.ts
@@ -1,10 +1,21 @@
-import { io } from 'socket.io-client';
-import config from '@/config';
-const socketURL = 'ws://localhost:5005';
-console.log(socketURL)
-console.log(config.SOCKET_URL)
-const socket = io(socketURL, {
-    reconnectionDelayMax: 10000,
-})
+// import { io } from 'socket.io-client';
+// import config from '@/config';
+// const socketURL = 'ws://localhost:5005';
+// console.log(socketURL)
+// console.log(config.SOCKET_URL)
+// const socket = io(socketURL, {
+//     reconnectionDelayMax: 10000,
+// })
 
-export default socket
+// export default socket
+
+import { io } from 'socket.io-client';
+const socketURL = 'wss://atomica.live/collaboration';  // Public WebSocket URL via the proxy
+console.log(socketURL);
+
+const socket = io(socketURL, {
+  reconnectionDelayMax: 10000,
+  transports: ['websocket'],  // Use WebSocket only
+});
+
+export default socket;
