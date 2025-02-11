f7a9f5488c2cb90a167a56fae5fc77e86104ccba
Abin Babu <abinbabuonline@gmail.com>
2025-02-11 22:27:45 +0530

    fix : socket.io bug

--- client/src/lib/socket.ts
+++ client/src/lib/socket.ts
@@ -1,22 +1,22 @@
-// import { io } from 'socket.io-client';
-// import config from '@/config';
-// const socketURL = 'ws://localhost:5005';
-// console.log(socketURL)
-// console.log(config.SOCKET_URL)
-// const socket = io(socketURL, {
-//     reconnectionDelayMax: 10000,
-// })
-
-// export default socket
-
 import { io } from 'socket.io-client';
-const socketURL = 'https://atomica.live/collaboration';
-
+import config from '@/config';
+const socketURL = 'ws://localhost:5005';
+console.log(socketURL)
+console.log(config.SOCKET_URL)
 const socket = io(socketURL, {
     reconnectionDelayMax: 10000,
-    // path: "/collaboration",
-    transports: ['websocket'], // Ensure it's using WebSocket for communication
-});
+})
 
-export default socket;
+export default socket
+
+// import { io } from 'socket.io-client';
+// const socketURL = 'https://atomica.live/collaboration';
+
+// const socket = io(socketURL, {
+//     reconnectionDelayMax: 10000,
+//     // path: "/collaboration",
+//     transports: ['websocket'], // Ensure it's using WebSocket for communication
+// });
+
+// export default socket;
 
