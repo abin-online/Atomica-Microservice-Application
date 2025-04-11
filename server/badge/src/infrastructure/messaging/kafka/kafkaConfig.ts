d199f1e4f1a859f0f7db3beabb9a0431a5e14ba6
Abin Babu <abinbabuonline@gmail.com>
2025-04-11 17:05:30 +0530

    chores : kafka connection timeout

--- server/badge/src/infrastructure/messaging/kafka/kafkaConfig.ts
+++ server/badge/src/infrastructure/messaging/kafka/kafkaConfig.ts
@@ -1,7 +1,11 @@
 import { Kafka } from "kafkajs";
 const kafka = new Kafka({
     clientId: "badge-service",
-    brokers: ['kafka-service:9092']
+    brokers: ['kafka-service:9092'],
+    connectionTimeout: 5000,
+    retry: {
+        retries: 10
+    }
 })
 
 export default kafka
--- server/test-service/src/infrastructure/messaging/kafka/kafkaConfig.ts
+++ server/test-service/src/infrastructure/messaging/kafka/kafkaConfig.ts
@@ -3,6 +3,10 @@ import { Kafka } from "kafkajs";
 const kafka = new Kafka({
     clientId: "test-service",
     brokers: ['kafka-service:9092'],
-});
+    connectionTimeout: 5000,
+    retry: {
+        retries: 10
+    }
+})
 
-export default kafka;
+export default kafka
