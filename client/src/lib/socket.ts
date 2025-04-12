a9933bc88bcda6de984e3c27c63fa0636eda9cc6
Abin Babu <abinbabuonline@gmail.com>
2025-04-12 09:28:33 +0530

    fix : socket URL

--- client/src/lib/socket.ts
+++ client/src/lib/socket.ts
@@ -1,9 +1,6 @@
 import { io } from 'socket.io-client';
-import config from '@/config';
 
-console.log("🔌 Using SOCKET_URL:", config.SOCKET_URL);
-
-const socket = io('wss://atomica.live/socket.io', {
+const socket = io('wss://atomica.live', {
   reconnectionDelayMax: 10000,
   transports: ['websocket'],
   path: '/socket.io', 
