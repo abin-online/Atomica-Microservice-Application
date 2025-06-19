56acf39e5d0d9c3263c664d9b4f0d3e3d55cd03d
Abin Babu <abinbabuonline@gmail.com>
2025-06-19 12:45:38 +0530

    chores : entry point

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -2,13 +2,23 @@
 
 import React, { useState, useEffect } from 'react';
 
+import Link from 'next/link';
+import { useAppDispatch, useAppSelector } from '@/lib/hook';
+import { RootState } from '@/lib/store';
+import { removeUser } from '@/lib/features/users/userSlice';
+import { useRouter } from 'next/navigation';
+import { logOut } from '@/api/userAuthentication';
+import toast from 'react-hot-toast';
+import { useConfirmationDialog } from './customHooks/useConfirmationDialog';
 import BannerSlider from '@/components/user/Banner';
 import LeaderBoard from '@/components/user/LeaderBoard';
 import Marqueue from '@/components/user/Marqueue';
+import NavBar from '@/components/user/Navbar';
 
 const HomePage = ()=> {
   return (
     <>
+    <NavBar/>
     <Marqueue/>
     <BannerSlider/>
     <LeaderBoard/>
